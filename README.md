925cbe8b7ba3eb5d696a7163fa64573f6bb939b7
Ksar <hcwenyc@hotmail.com>
2018-05-18 15:02:20 -0700

    Use svpng to show results

--- README.md
+++ README.md
@@ -1,50 +1,7 @@
 # Sptracing
-
-depth rendering ![](https://img.shields.io/badge/build-passing-brightgreen.svg)  
-reflection ![](https://img.shields.io/badge/build-building-yellow.svg)  
-directional light ![](https://img.shields.io/badge/build-building-yellow.svg)  
-point light light ![](https://img.shields.io/badge/build-building-yellow.svg)  
-spot light ![](https://img.shields.io/badge/build-building-yellow.svg)  
-material demo ![](https://img.shields.io/badge/build-building-yellow.svg)
-
-## Rendering result(Depth)
-![result](img/result.png)
-
-## Install SDL2
-
-### Linux & macOS
-
-- Download `SDL2` from [here](https://www.libsdl.org/release/SDL2-2.0.5.tar.gz).
-
-- Unzip to `./SDL2-2.0.5` 
-
-- Install
-
-  ```bash
-  cd SDL2-2.0.5
-  make
-  make install
-  ```
-
-## Windows
-
-#### Download
-
-- Download `SDL2` from [here](https://www.libsdl.org/release/SDL2-devel-2.0.5-VC.zip).
-- Unzip to `./SDL2-2.0.5`
-
-#### Copy .dll files
-
-There two ways to copy `.dll` files:
-
-##### In the project directroy
-
-- copy the x86 file to `./debug/x86`
-- copy the x64 file to `./debug/x64`
-
-In system dirctories
-
-- x86: `C:/Windows/system32`
-- x64: `C:/Windows/SysWOW64`
-
-Then you can run it on Visual Studio 2017
+A Ray Tracing renderer base on [svpng].(https://github.com/miloyip/svpng)
+# Rendering result
+### Depth
+![D-result](img/depth_test.png)
+### Normal
+![N-result](img/normal_test.png)
--- main.cpp
+++ main.cpp
@@ -1,82 +1,89 @@
-#include "SDL.h"
-#include "Detile/Sphere.hpp"
-#include "Detile/Camera_F.hpp"
+#include "lib/svpng.inc"
+#include "Detail/Sphere.hpp"
+#include "Detail/Camera_F.hpp"
 
 #include <algorithm>
 
-const int height = 800;
-const int width = 800;
+const int height = 512;
+const int width = 512;
 
-SDL_Window* win = nullptr;
-SDL_Surface* surface = nullptr;
 
-int init()
+void render_depth(Sphere scene, Camera_F camera, int max_depth)
 {
-    if (SDL_Init(SDL_INIT_EVERYTHING) == -1)
-        return 1;
-
-    win = SDL_CreateWindow("Sptracing", 100, 100, width, height, SDL_WINDOW_SHOWN);
-
-    if (win == nullptr)
-        return 1;
-
-    surface = SDL_GetWindowSurface(win);
-
-    if (surface == nullptr)
-        return 2;
-
-    return 0;
-}
-
-void Destroy()
-{
-    SDL_FreeSurface(surface);
-    SDL_DestroyWindow(win);
-    SDL_Quit();
-}
-
-void render_test(Sphere scene, Camera_F camera, int max_depth)
-{
-    SDL_LockSurface(surface);
-    auto pixels = (unsigned char*)surface->pixels;
-
+	unsigned char depth_test[width * height * 3];
+	auto pixels = depth_test;
+	FILE *fp = fopen("img/depth_test.png", "wb");
+    
     camera.initialize();
 
     float dD=255.0f / max_depth;
-
+	
+	int i = 0;
     for (int y = 0; y < height; y++)
     {
         float sy = 1 - y * 1.0f / height;
-        for (int x = 0; x < width; ++x) {
+        for (int x = 0; x < width; x++) {
             float sx = 1 - x * 1.0f / width;
             Ray3 ray = camera.generate_ray(sx, sy);
             Intersect_result result = scene.intersect(ray);
-            if (result.geometry)
-            {
-                auto depth = (unsigned char)( 255 - std::min(result.distance * dD, 255.0f) );
-                pixels[4 * (y * surface->w + x) + 0] = depth;
-                pixels[4 * (y * surface->w + x) + 1] = depth;
-                pixels[4 * (y * surface->w + x) + 2] = depth;
-                pixels[4 * (y * surface->w + x) + 3] = 255;
-            }
+			if (result.geometry)
+			{
+				auto depth = (unsigned char)(255 - std::min(result.distance * dD, 255.0f));
+				pixels[i	] = depth;
+				pixels[i + 1] = depth;
+				pixels[i + 2] = depth;
+			}
+			else pixels[i] = pixels[i + 1] = pixels[i + 2] = 0;
+			
+			i += 3;
         }
     }
 
-    SDL_UnlockSurface(surface);
-    SDL_UpdateWindowSurface(win);
+	svpng(fp, width, height, depth_test, false);
+	fclose(fp);
+}
+
+void render_normal(Sphere scene, Camera_F camera)
+{
+	unsigned char normal_test[width * height * 3], *pixels = normal_test;
+	FILE *fp = fopen("img/normal_test.png", "wb");
+
+	camera.initialize();
+
+	int i = 0;
+	for (int y = 0; y < height; y++)
+	{
+		float sy = 1 - y * 1.0f / height;
+		for (int x = 0; x < width; x++) {
+			float sx = 1 - x * 1.0f / width;
+			Ray3 ray = camera.generate_ray(sx, sy);
+			Intersect_result result = scene.intersect(ray);
+			if (result.geometry)
+			{
+				pixels[i	] = (result.normal.x + 1) * 128;
+				pixels[i + 1] = (result.normal.y + 1) * 128;
+				pixels[i + 2] = (result.normal.z + 1) * 128;
+			}
+			else pixels[i] = pixels[i + 1] = pixels[i + 2] = 0;
+
+			i += 3;
+		}
+	}
+
+	svpng(fp, width, height, normal_test, false);
+	fclose(fp);
 }
 
 int main(int argc, char *argv[])
 {
+	Sphere sp(Vector3(0, 10, -10), 10);
+	Camera_F caf(Vector3(0, 10, 10), 
+				 Vector3(0, 0, -1), 
+				 Vector3(0, 1, 0), 90);
+	
+	render_depth(sp, caf, 20);
 
-    if (init() != 0)
-        return -1;
+	render_normal(sp, caf);
 
-
-    render_test(Sphere(Vector3(0, 10, -10), 10),
-                Camera_F(Vector3(0, 10, 10), Vector3(0, 0, -1), Vector3(0, 1, 0), 90), 20);
-
-    SDL_Delay(10000);
-    Destroy();
     return 0;
 }
